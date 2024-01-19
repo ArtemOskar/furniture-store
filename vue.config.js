3d60ee2c5325da12a7a2c3fc5326de00d5ed3da0
Artem Molokov <frontenddevelopment.molokov@gmail.com>
2024-01-19 05:25:07 +0200

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     // publicPath: `/furniture-store/`,
-    publicPath: process.env.NODE_ENV === 'production' ? '/furniture-store/' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/furniture-store' : '/',
     transpileDependencies: true,
 })
 
