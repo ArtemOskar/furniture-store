0837e6be8d5478c164a8e59c2789d79c85d932b1
Artem Molokov <frontenddevelopment.molokov@gmail.com>
2024-01-19 02:53:31 +0200

    Update package.json

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,5 @@
-// const { defineConfig } = require("@vue/cli-service");
-// module.exports = defineConfig({
-//     publicPath: `/furniture-store/`,
-//     transpileDependencies: true,
-// })
-
 const { defineConfig } = require('@vue/cli-service')
-
 module.exports = defineConfig({
-    publicPath: process.env.NODE_ENV === 'production' ? '/furniture-store/' : '/',
-    outputDir: 'docs',
-    filenameHashing: false,
-    chainWebpack: (config) => {
-        config.module.rules.delete('eslint')
-    },
+    publicPath: `/furniture-store/`,
+    transpileDependencies: true,
 })
