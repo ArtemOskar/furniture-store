fef83fe9e5f53671a17453634068727c93d68724
Artem Molokov <frontenddevelopment.molokov@gmail.com>
2024-01-19 02:26:11 +0200

    update router

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
-const { defineConfig } = require("@vue/cli-service");
+// const { defineConfig } = require("@vue/cli-service");
+// module.exports = defineConfig({
+//     publicPath: `/furniture-store/`,
+//     transpileDependencies: true,
+// })
+
+const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-    publicPath: `/furniture-store/`,
-    transpileDependencies: true,
+    publicPath: process.env.NODE_ENV === 'production' ? '/furniture-store/' : '/',
+    outputDir: 'docs',
+    filenameHashing: false,
+    chainWebpack: (config) => {
+        config.module.rules.delete('eslint')
+    },
 })
